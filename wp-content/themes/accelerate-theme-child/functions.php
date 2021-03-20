03c7d4901988c968b73a33abaaf41c22fe333c75
sharp_amy <stuffamydoes@gmail.com>
2021-03-20 16:57:51 -0400

    begin styling icons in footer

--- wp-content/themes/accelerate-theme-child/functions.php
+++ wp-content/themes/accelerate-theme-child/functions.php
@@ -16,3 +16,8 @@ function accelerate_child_scripts(){
 	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'accelerate-style' ));
 }
 add_action( 'wp_enqueue_scripts', 'accelerate_child_scripts' );
+
+add_action( 'wp_enqueue_scripts', 'enqueue_load_fa' );
+	function enqueue_load_fa() {
+	wp_enqueue_style( 'load-fa', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' );
+	}
